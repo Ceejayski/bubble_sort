372b904640a740084b77e15ad63bc854cebb36b0
Ceejayski <61045640+Ceejayski@users.noreply.github.com>
2020-10-05 10:19:00 +0100

    Create bubble_sort.rb
    Create bubble_sort method
    
    Added an if argument if array length is > 2
    
    Attached primary while loop to it

---
+++ bubble_sort.rb
@@ -0,0 +1,11 @@
+def bubble_sort(array)
+    # to check the if the array length is greater than 2 
+    if array.size >= 2
+        swap = true
+        # while loop to run as long as swap is true 
+        while swap
+        end
+    else
+        print array
+    end
+end
